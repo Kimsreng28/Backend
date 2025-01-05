8bb12cb7dbc802ad3e905d66dbc6a6e3cb0ddd85
Kimsreng28 <kimsrenghai@gmail.com>
2025-01-05 18:24:22 +0700

    Add

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,19 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="text-sm self-start text-gray-400">Kim</a>
+        <h3 class="text-xl mt-3 font-bold group-hover:text-blue-800 transition-colors duration-300">Mobile Development
+        </h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $5000.00</p>
+
+    </div>
+
+    <div class="space-x-1">
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</x-panel>
---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<x-panel class=" flex flex-col text-center">
+
+    <div class="text-sm self-start">Kim</div>
+    <div class="py-8 ">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Mobile Development</h3>
+        <p class="text-sm mt-4">Full Time - From $5000.00</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div class="space-x-1">
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+        <x-employer-logo :width="42" />
+    </div>
+
+</x-panel>
